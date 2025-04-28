c4b66c2f9b9a1f47d4359e5c3ff8d48b2a3670d7
LeviKS1 <lk617483@wne.edu>
2025-04-28 05:43:27 +0000

    clean: refactor main

--- src/main/java/RomanNumeral.java
+++ src/main/java/RomanNumeral.java
@@ -4,34 +4,29 @@ public class RomanNumeral {
 
     String convert (int input){
         String message = "";
-        int ones = input % 10;
-        int nine = input % 9;
-        int five = input % 5;
-        int four = input % 4;
 
-
-        if (input == 10){
-            int q = (int) Math.floorDiv(input, 10);
+        if (input >= 10){
+            int q = Math.floorDiv(input, 10);
             for (int i = 0; i < q; i++){
                 message += "X";
+                input -= 10;
             }
         }
-        if (nine == 0){
+
+        if (input == 9){
             message += "IX";
         }
         else if (input == 5){
             message += "V";
         }
-        else if (four == 0){
+        else if (input == 4){
             message += "IV";
         }
         else{
-            for (int i = 0; i < ones; i++){
+            for (int i = 0; i < input; i++){
                 message += "I";
             }
-        }
-        
-        
+        }     
 
         return message;
 
