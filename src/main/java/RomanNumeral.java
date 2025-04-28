b8a2255aafeb61b77168a23933858d3270ed9fe4
LeviKS1 <lk617483@wne.edu>
2025-04-28 04:43:18 +0000

    code: five is V

--- src/main/java/RomanNumeral.java
+++ src/main/java/RomanNumeral.java
@@ -5,9 +5,18 @@ public class RomanNumeral {
     String convert (int input){
         String message = "";
         int ones = input % 10;
-        for (int i = 0; i < ones; i++){
-            message += "I";
+        int five = input % 5;
+
+        if (five == 0){
+            message += "V";
         }
+        else{
+            for (int i = 0; i < ones; i++){
+                message += "I";
+            }
+        }
+        
+        
 
         return message;
 
