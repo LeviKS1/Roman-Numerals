66134ff1be468022bad8016e1bde774eeb58da73
LeviKS1 <lk617483@wne.edu>
2025-04-28 04:50:37 +0000

    code: four is IV

--- src/main/java/RomanNumeral.java
+++ src/main/java/RomanNumeral.java
@@ -6,9 +6,13 @@ public class RomanNumeral {
         String message = "";
         int ones = input % 10;
         int five = input % 5;
+        int four = input % 4;
 
         if (five == 0){
             message += "V";
+        }
+        else if (four == 0){
+            message += "IV";
         }
         else{
             for (int i = 0; i < ones; i++){
